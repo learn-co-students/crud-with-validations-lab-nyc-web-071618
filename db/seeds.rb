634ecac215f33c03760b6d35a72ed201519689c5
trisjones <trisjones04@gmail.com>
2018-08-13 15:43:07 -0400

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,31 @@
+Song.create!(
+  title: "Karma Police",
+  released: true,
+  release_year: 1995,
+  artist_name: "Radiohead",
+  genre: "Alt Rock"
+)
+
+Song.create!(
+  title: "Wait",
+  released: true,
+  release_year: 2016,
+  artist_name: "M83",
+  genre: "Alt Rock"
+)
+
+Song.create!(
+  title: "Crossfire",
+  released: true,
+  release_year: 2011,
+  artist_name: "Brandon Flowers",
+  genre: "Indie Rock"
+)
+
+Song.create!(
+  title: "All These Things That I've Done",
+  released: true,
+  release_year: 2010,
+  artist_name: "The Killers",
+  genre: "Indie Rock"
+)
