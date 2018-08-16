00f0a32f837ddc7485a1052678b8b9db2a3f1526
Andrew Och <andrewoch95@gmail.com>
2018-08-16 17:28:31 -0400

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+song1 = Song.create(title: 'Homecoming', released: true, release_year: 2005, artist_name: 'Kanye', genre: 'Rap')
+song2 = Song.create(title: 'Yoo', released: true, release_year: 2014, artist_name: 'Andy', genre: 'Pop')
+song3 = Song.create(title: "Dancing in the Moonlight", released: false, release_year: 2019, artist_name: "Hall & Oats", genre: "Fire")
